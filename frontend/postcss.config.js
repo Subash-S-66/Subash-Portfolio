645c7ec60caa6877236d49233a7b84f594ac3fab
subash-s-66 <subash.93450@gmail.com>
2025-12-07 00:24:11 +0530

    chore: update frontend build assets

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,4 +1,6 @@
-export default {
+// Use CommonJS exports so PostCSS can load this config in environments
+// that run Vite as a CommonJS module (Docker builds / older Node setups).
+module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
