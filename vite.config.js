a9874a9a5154505b1c07c98034e89cb2d5962c86
subash-s-66 <subash.93450@gmail.com>
2025-12-06 16:24:52 +0530

    Remove root from vite config and fix build script paths

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Subash-Portfolio/',
-  root: './frontend',
   server: {
     port: 3000,
     proxy: {
